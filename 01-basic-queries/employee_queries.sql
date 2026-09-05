4ff6d577101d2a66447467d2cee647bbe2f06c6a
Mrunal khairnar <156316224+Mrunalkhairnar5@users.noreply.github.com>
2026-09-05 12:33:30 +0530

    Create employee_queries.sql
    Add basic employee SQL queries

---
+++ 01-basic-queries/employee_queries.sql
@@ -0,0 +1,43 @@
+CREATE DATABASE hitesh;
+USE hitesh;
+
+CREATE TABLE employee (
+    dept_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    department VARCHAR(50),
+    salary DECIMAL(10,2) CHECK (salary > 0),
+    email VARCHAR(100) UNIQUE,
+    city VARCHAR(50)
+);
+
+INSERT INTO employee VALUES
+(1, 'Amit', 'IT', 50000, 'amit@gmail.com', 'Pune'),
+(2, 'Rahul', 'HR', 60000, 'rahul@gmail.com', 'Mumbai'),
+(3, 'Neha', 'IT', 70000, 'neha@gmail.com', 'Pune'),
+(4, 'Priya', 'Finance', 80000, 'priya@gmail.com', 'Nashik'),
+(5, 'Karan', 'IT', 60000, 'karan@gmail.com', 'Pune');
+
+SELECT * FROM employee;
+
+SELECT *
+FROM employee
+WHERE department = 'IT';
+
+SELECT name, salary
+FROM employee
+WHERE salary > 60000;
+
+SELECT DISTINCT department
+FROM employee;
+
+SELECT *
+FROM employee
+ORDER BY salary DESC;
+
+SELECT *
+FROM employee
+ORDER BY salary DESC
+LIMIT 1 OFFSET 1;
+
+SELECT MAX(salary)
+FROM employee;
